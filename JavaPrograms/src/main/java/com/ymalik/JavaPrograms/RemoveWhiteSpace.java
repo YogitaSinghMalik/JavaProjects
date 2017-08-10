84755c6f69190c7cdb0e078b3c228fe2ca44c8fd
Yogita Singh Malik <yogitasinghmalik@gmail.com>
2017-08-10 23:34:52 +0530

    Adding program which removes extra white spaces from String.

---
+++ JavaPrograms/src/main/java/com/ymalik/JavaPrograms/RemoveWhiteSpace.java
@@ -0,0 +1,12 @@
+package com.ymalik.JavaPrograms;
+
+public class RemoveWhiteSpace 
+{
+	public static void main (String a[])
+		{
+		String mystring = "      My Name is Yogita    ";
+		System.out.println(mystring.trim());
+	
+	}
+
+}
