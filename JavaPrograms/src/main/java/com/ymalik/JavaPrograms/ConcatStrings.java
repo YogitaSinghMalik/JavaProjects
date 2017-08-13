4680ea7340ba01706ac05117686294a4bd24422e
Yogita Singh Malik <yogitasinghmalik@gmail.com>
2017-08-13 12:04:25 +0530

    Program to Concatenate two strings

---
+++ JavaPrograms/src/main/java/com/ymalik/JavaPrograms/ConcatStrings.java
@@ -0,0 +1,13 @@
+package com.ymalik.JavaPrograms;
+
+public class ConcatStrings {
+	public static void main(String arg[])
+	{
+		String a = "My name is ";
+		String b = "Yogita";
+		String c = a.concat(b);
+		System.out.println(c);
+		
+	}
+
+}
